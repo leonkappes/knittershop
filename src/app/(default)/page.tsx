273954aee786cd8ef91e96f281acd307bd452592
Leon Kappes <leon@kappes.space>
2023-11-01 22:07:41 +0100

    Project upload

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,12 +1,9 @@
-'use client'
 import Hero from "@/components/Default/Hero";
 
 export default function Home() {
   return (
       <>
-
         <Hero showButton={true} />
-        <Hero showButton={false}/>
       </>
   )
 }
--- src/components/Default/Hero.tsx
+++ src/components/Default/Hero.tsx
@@ -1,18 +1,10 @@
 import HeroImage from "../../assets/nachhaltige-mode-nordlicht162.jpg.webp"
 import Image from "next/image";
+import Link from "next/link";
 
-interface HeroProps {
-    showButton: boolean
-    callbackFunction: () => void
-}
-export default function Hero({showButton = true, callbackFunction}: HeroProps) {
+export default function Hero({hrefAttribute = '#'}) {
 
     // TODO: Fix Client and Server Click Handeling
-    function handleClick() {
-        console.log("pressed")
-        showButton = false
-        callbackFunction()
-    }
 
     return (
         <div className="h-screen overflow-hidden">
@@ -25,11 +17,10 @@ export default function Hero({showButton = true, callbackFunction}: HeroProps) {
                 <p className="text-lg text-primary w-full bg-gradient-to-r from-background to-transparent to-70% p-4 pr-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl rhoncus mattis rhoncus urna. Libero justo laoreet sit amet cursus sit. Consequat ac felis donec et odio pellentesque diam.</p>
             </div>
 
-            { showButton ? (
-                <div className="fixed bottom-0 w-full flex items-center justify-center mb-24">
-                    <button className="text-xl text-white bg-accent rounded-full py-2 px-4" onClick={handleClick}>Los geht&apos;s</button>
-                </div>
-            ): (<></>)}
+
+            <div className="self-end w-full flex items-center justify-center mb-8">
+                <Link href={hrefAttribute} className="text-lg text-white bg-accent rounded-full py-2 px-4 font-serif">Los geht&apos;s</Link>
+            </div>
 
         </div>
     )
