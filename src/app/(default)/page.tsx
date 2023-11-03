c4187df790625dbce42713873eee7a34bef50cab
mauveyone <lennart@muenter.eu>
2023-11-03 20:30:03 +0100

    newFooter

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Default/Hero";
+import Footer from "@/components/Default/Footer";
 
 export default function Home() {
   return (
       <>
         <Hero showButton={true} />
+        <Footer />
       </>
   )
 }
---
+++ src/components/Default/Footer.tsx
@@ -0,0 +1,24 @@
+import { PiInstagramLogoLight } from "react-icons/pi";
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <div className="w-full bg-background p-2">
+        <div className="flex flex-row justify-evenly gap-4 mb-2">
+            <Link href={""} className="text-md text-primary font-serif text-center w-1/3">agb</Link>
+            <Link href={""} className="text-md text-primary font-serif text-center w-1/3">impressum</Link>
+            <Link href={""} className="text-md text-primary font-serif text-center w-1/3">versand</Link>
+        </div>
+        <div className="flex flex-row justify-center">
+            <Link href={""}>
+                <PiInstagramLogoLight className="fill-primary w-6 h-6"/>
+            </Link>
+        </div>
+        </div>
+    )
+}
+
+/*
+* Future Icons: PiHeartThin, PiHandbagSimpleThin, PiHouseThin, PiInfoThin, PiMagnifyingGlassMinusThin, PiMagnifyingGlassPlusThin, PiMagnifyingGlassThin,
+* PiNotePencilThin, PiPackageThin, PiPaperPlaneTiltThin,
+* */
